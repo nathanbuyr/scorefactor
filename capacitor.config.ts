b0749010070a3e577a948805b1dc09c04a47b1cc
nathanbuyr <G00421082@atu.ie>
2025-05-21 01:15:33 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'scorefactor',
+  webDir: 'www'
+};
+
+export default config;
